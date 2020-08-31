8d83fd2d21856b436125326d8ce0332e83039d03
StevenArdis <steven_ardis@hotmail.com>
2020-08-31 21:19:03 +0800

    添加项目文件。

---
+++ WpfApp1/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WpfApp1
+{
+    class Student : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set 
+            { 
+                name = value;
+                if (this.PropertyChanged != null)
+                {
+                    this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Name"));
+                }
+            }
+        }
+
+    }
+}
